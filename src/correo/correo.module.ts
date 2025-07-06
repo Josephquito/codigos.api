5652350e384242c9bfe5707c07945bcf7d0e5cf0
Josephquito <jdquito.3@sudamericano.edu.ec>
2025-07-06 01:21:59 -0500

    back verificar tipo de correo

--- src/correo/correo.module.ts
+++ src/correo/correo.module.ts
@@ -4,8 +4,10 @@ import { CorreoController } from './correo.controller';
 import { ImapService } from '../imap/imap.service';
 import { GmailService } from '../gmail/gmail.service';
 import { ImapAccountService } from '../imap-account/imap-account.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [CorreoController],
   providers: [CorreoService, ImapService, GmailService, ImapAccountService],
 })
