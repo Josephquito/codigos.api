3aadd71260500d700e23d349b7d7818cab72c835
Josephquito <jdquito.3@sudamericano.edu.ec>
2025-07-06 20:14:49 -0500

    back clavexplataroma

--- src/imap/imap.module.ts
+++ src/imap/imap.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ImapService } from './imap.service';
 import { ImapController } from './imap.controller';
 import { ImapAccountModule } from '../imap-account/imap-account.module'; // 👈 Asegúrate de importar esto
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [ImapAccountModule], // 👈 Importarlo aquí
+  imports: [ImapAccountModule, AuthModule], // 👈 Importarlo aquí
   providers: [ImapService],
   controllers: [ImapController],
 })
