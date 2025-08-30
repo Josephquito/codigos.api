b66165fad1746f33c87ca5a4c6490bead2216688
Josephquito <jdquito.3@sudamericano.edu.ec>
2025-08-30 01:00:02 -0500

    CORS jotavix.com

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://codigos.jotavix.com'],
+    origin: [
+      'http://localhost:4200',
+      'https://jotavix.com',
+      'http://jotavix.com',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT || 3000);
